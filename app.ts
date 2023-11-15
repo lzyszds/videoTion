8c9bcb22c5fa05f299c9d33ede311fa6ee4b90bc
lzyszds <1024327189@qq.com>
2023-11-15 10:35:08 +0800

    去除注释

--- app.ts
+++ app.ts
@@ -6,12 +6,12 @@ import axios from "axios";
 import { Worker } from "worker_threads";
 import { merge } from './merge'
 import https from 'https'
-import bodyParser from 'body-parser'
 import dayjs from 'dayjs'
 
 
 const getVideo = require("./getVideo");
 
+// getCoverImg('IPZZ-102', 'IPZZ-102 おじさん大好き新卒部下の密着ささやき誘惑を受け続け、5日目の金曜日に完堕ちしたオレ 桃乃木かな','https://ver1.sptvp.com/poster/C/30/94zZeqfZnmnsAVYmEKVF-55.png')
 
 const app = express();
 
@@ -20,10 +20,10 @@ app.set('views', './template');
 app.engine('html', swig.renderFile);
 //设置渲染引擎为html
 app.set('view engine', 'html');
-app.use(express.urlencoded({ extended: false }))
+// app.use(express.urlencoded({ extended: true }))
 // 使用 body-parser 中间件来解析请求体
-app.use(bodyParser.json()); // 解析 JSON 格式的请求体
-app.use(bodyParser.urlencoded({ extended: true })); // 解析 URL 编码的请求体
+app.use(express.json()); // 解析 JSON 格式的请求体
+app.use(express.urlencoded({ extended: true })); // 解析 URL 编码的请求体
 
 
 app.get("/", (req, resp) => {
@@ -33,7 +33,11 @@ app.get("/", (req, resp) => {
     //使用正则表达式去掉空格等特殊字符
     const reg = /[\s\[\]\(\)\{\}\+\*\.\?\！\|\\\/\:\;\~\`\!\@\#\$\%\^\&\=\,\<\>\"\'\·]/g
     const newName = name.replaceAll(reg, '').replaceAll(' ', '')
-    fs.renameSync(`./public/cover/${file}`, `./public/cover/${newName}.jpg`)
+    fs.renameSync(`./public/cover/${name}.jpg`, `./public/cover/${newName}.jpg`)
+    fs.access(`./public/cover/${name}.png`, fs.constants.F_OK, (err) => {
+      if (err) return;
+      fs.renameSync(`./public/cover/${name}.png`, `./public/cover/${newName}.png`)
+    })
   })
 
   //将文件名中的空格去掉 ,特殊字符也去掉
@@ -47,25 +51,34 @@ app.get("/", (req, resp) => {
 
 
   const coverList = fs.readdirSync('./public/cover').map((file: any) => {
-    const name = file.split('.')[0]
-    const url = name.replaceAll(" ", '')
-    const stat = fs.statSync('./public/videoDownload/' + file.replace('.jpg', '.mp4'))
-    let datails: { time: string, size: string } = {
-      time: dayjs(stat.birthtimeMs).format("YYYY-MM-DD HH:mm"),
-      size: formatFileSize(stat.size),
-    }
-    return {
-      stampTime: stat.birthtimeMs,
-      name: name,
-      img: `http://localhost:3000/cover/${file}`,
-      url: `http://localhost:3000/videoDownload/${url}.mp4`,
-      datails
+    if (file.indexOf('.png') == -1) {
+      const name = file.split('.')[0]
+      const url = name.replaceAll(" ", '')
+      let stat = null, datails = null
+      try {
+        stat = fs.statSync(`./public/videoDownload/${name}.mp4`)
+        datails = {
+          time: dayjs(stat.birthtimeMs).format("YYYY-MM-DD HH:mm"),
+          size: formatFileSize(stat.size),
+        }
+      } catch (e) {
+        console.log(`lzy  e:`, e)
+      }
+      return {
+        stampTime: stat ? stat!.birthtimeMs : null,
+        name: name,
+        img: `http://localhost:3000/cover/${file}`,
+        cover: `http://localhost:3000/cover/${name}.png`,
+        url: `http://localhost:3000/videoDownload/${url}.mp4`,
+        datails
+      }
     }
   })
 
   resp.render('index', {
-    coverlist: quickSortByTimestamp(coverList, 'stampTime', false)
+    coverList: quickSortByTimestamp(coverList.filter((res) => res), 'stampTime', false)
   });
+  //: quickSortByTimestamp(coverList, 'stampTime', false)
 })
 app.get('/python', (req, res) => {
   res.render('python')
@@ -83,12 +96,13 @@ app.post("/test", async (req, res) => {
 */
 app.post("/search", async (req, res) => {
   if (JSON.stringify(req.body) === '{}') return res.send('请求参数不能为空,找不到请求参数');
-  let { url, thread, headers, name } = req.body;
+  let { url, thread, headers, name, cover } = req.body;
+  cover = cover.match(/"([^"]+)"/)[1]
   name = name.replace('[无码破解]', '')
   //截取番号出来
   const id = getIdNumber(name)
   //替换名字非法字符 保留日语和中文字符，并删除其他非字母数字字符
-  name = name.replace(/[^\u4E00-\u9FA5\u3040-\u309F\u30A0-\u30FF\uFF65-\uFF9Fa-zA-Z0-9]/g, '')
+  name = name.replace(/[^\u4E00-\u9FA5\u3040-\u309F\u30A0-\u30FF\uFF65-\uFF9Fa-zA-Z0-9/-]/g, '')
   headers = JSON.parse(headers)
   Object.assign(headers, {
     "accept": "*/*",
@@ -113,17 +127,23 @@ app.post("/search", async (req, res) => {
 
   const dataArr = m3u8Data.split('\n').filter((res: any) => res.indexOf(videoName) === 0)
   const countArr = splitArrayIntoEqualChunks(dataArr, thread)
+  let isFirstCertificate = false
+  getVideo(countArr[0], 0, 0, urlPrefix, headers)
+    .then()
+    .catch((e: any) => {
+      if (e.indexOf('unable to verify the first certificate') != -1) {
+        console.log('无法验证第一个证书')
+        isFirstCertificate = true
+      } else {
+        console.log(e)
+      }
+    }).finally(() => {
+      downLoadPlan++
+    })
 
-  getVideo(countArr[0], 0, 0, urlPrefix, headers).then((result: any) => {
-    console.log(`lzy  result,then:`, result)
-  }).catch((e: any) => {
-    console.log(e, 'catch');
-  }).finally((res: any) => {
-    downLoadPlan++
-  })
-
-  getCoverImg(id, name)//获取封面图片
-
+  await sleep(10 * 1000) //阻塞20秒
+  if (isFirstCertificate) return res.send('无法验证第一个证书')
+  let getCoverIndex = 0 //第几次尝试下载图片的索引
   for (let i = 1; i < thread; i++) {
     const seprateThread = new Worker(__dirname + `/seprate/seprateThread${i}.js`);
     seprateThread.on("message", async () => {
@@ -135,6 +155,8 @@ app.post("/search", async (req, res) => {
         if (downLoadPlan >= 15) {
           merge(name).then(resultext => {
             res.send('合体成功，但是有部分视频没有下载完全')
+            if (resultext === '合成成功')
+              getCoverImg(id, name, cover, getCoverIndex)//获取封面图片
           })
           return
         }
@@ -143,15 +165,14 @@ app.post("/search", async (req, res) => {
         merge(name).then(resultext => {
           timer && clearTimeout(timer)
           res.send(resultext)
+          if (resultext === '合成成功')
+            getCoverImg(id, name, cover, getCoverIndex)//获取封面图片
         })
       }
     });
     seprateThread.postMessage({ urlData: countArr[i], i, headers, urlPrefix });
   }
 })
-
-
-
 
 app.listen(3000, () => {
   console.log("http://localhost:3000");
@@ -176,7 +197,9 @@ function getIdNumber(val: string) {
   return val.slice(0, index)
 }
 
-function getCoverImg(id: string, name: string) {
+function getCoverImg(id: string, name: string, cover2: string, getCoverIndex: number) {
+  let getHoverCoverIndex = 0 //第几次尝试下载hover图片的索引
+  if (getCoverIndex >= 5 || getHoverCoverIndex >= 5) return
   /* 获取图片，图片来自missav.com中，因为这个网站没做拦截 */
   const url = `https://cdn82.akamai-content-network.com/${id}/cover.jpg?class=normal`
   https.get(url, (response) => {
@@ -185,11 +208,30 @@ function getCoverImg(id: string, name: string) {
     response.pipe(fileStream);
     fileStream.on('finish', () => {
       fileStream.close();
-      console.log('图片下载完成');
+      console.log('图片下载完成1');
+
+      //下载第二张封面。hover中的封面
+      function getHoverCoverImg(index: number) {
+        https.get(cover2, (response) => {
+          const localPath = './public/cover/' + name + '.png'
+          const fileStream = fs.createWriteStream(localPath);
+          response.pipe(fileStream);
+          fileStream.on('finish', () => {
+            fileStream.close();
+            console.log('图片下载完成2');
+          });
+        }).on('error', (error) => {
+          getHoverCoverImg(++index)
+          console.error('(即将重试)下载出错:', error);
+        });
+      }
+      getHoverCoverImg(getHoverCoverIndex)
     });
   }).on('error', (error) => {
-    console.error('下载出错:', error);
+    getCoverImg(id, name, cover2, ++getCoverIndex)
+    console.error('(即将重试)下载出错:', error);
   });
+
 }
 //删除遗存的1.ts 2.ts这些未合成的视频文件
 function removeTsVideo() {
@@ -201,6 +243,15 @@ function removeTsVideo() {
     })
   }
 }
+
+function sleep(timer: number) {
+  return new Promise<string>((resolve, reject) => {
+    setTimeout(() => {
+      resolve('')
+    }, timer)
+  })
+}
+
 export function formatFileSize(fileSize: any) {
 
   const units = [
@@ -232,6 +283,7 @@ function quickSortByTimestamp(arr: any, key: string, isIncremental: boolean = tr
   const greater = [];
 
   for (const element of arr) {
+    if (!element) break;
     if (element[key] < pivot[key]) {
       less.push(element);
     } else if (element[key] > pivot[key]) {
--- seprate/seprateThread5.js
+++ seprate/seprateThread5.js
@@ -1,17 +1,16 @@
 
+
 const { parentPort } = require("worker_threads");
 const getVideo = require("../getVideo");
 parentPort.on("message", (limit) => {
   const { urlData, i, urlPrefix, headers } = limit;
   if (!urlPrefix || !urlData[i]) {
-    return parentPort.postMessage({ msg: e, result: '下载完成' })
+    return parentPort.postMessage({ msg: '1', result: '下载完成' })
   }
   try {
     getVideo(urlData, 0, i, urlPrefix, headers)
       .then(res => {
-        console.log(res);
       }).catch(e => {
-        console.log(e);
       }).finally(() => {
         parentPort.postMessage(i)
       })
